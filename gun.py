c72a6ee4142881967be4db903dac70df71a799d0
Alexander Ryabishkin <sintes4ek@gmail.com>
2022-07-25 13:35:28 +0300

    Change gun movement speed.

--- gun.py
+++ gun.py
@@ -3,13 +3,14 @@ import pygame
 
 class Gun():
 
-    def __init__(self, screen: object):
+    def __init__(self, screen):
         
         self.screen = screen
         self.image = pygame.image.load("images/gun.png")
         self.rect = self.image.get_rect()
         self.screen_rect = screen.get_rect()
         self.rect.centerx = self.screen_rect.centerx
+        self.center = float(self.rect.centerx)
         self.rect.bottom = self.screen_rect.bottom - 20
         self.mright = False
         self.mleft = False
@@ -26,7 +27,9 @@ class Gun():
         """
         # Right movement.
         if self.mright and self.rect.right < self.screen_rect.right:
-            self.rect.centerx += 1
+            self.center += 0.5
         # Left movement.
         elif self.mleft and self.rect.left > 0:
-            self.rect.centerx -= 1
+            self.center -= 0.5
+
+        self.rect.centerx = self.center
