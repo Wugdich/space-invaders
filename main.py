5b2287bc0abe1396c12d882a84ae80cf41caf6e2
Alexander Ryabishkin <sintes4ek@gmail.com>
2022-07-25 11:09:25 +0300

    Add main game cycle.

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
 import pygame
+import sys
 
+
+def run() -> None:
+    pygame.init()
+    screen = pygame.display.set_mode((1200, 800))
+    pygame.display.set_caption("Space Invaders")
+    bg_color = (0, 0, 0)
+
+    while True:
+        # Events proccessing.
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+        screen.fill(bg_color)
+        pygame.display.flip()
+
+
+if __name__ == '__main__':
+    run()
