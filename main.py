621b26d49b4899be6a823322d2342f7eb18b95a8
Alexander Ryabishkin <sintes4ek@gmail.com>
2022-07-25 11:41:15 +0300

    Add gun initialization.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import sys
+from gun import Gun
 
 
 def run() -> None:
@@ -7,6 +8,7 @@ def run() -> None:
     screen = pygame.display.set_mode((1200, 800))
     pygame.display.set_caption("Space Invaders")
     bg_color = (0, 0, 0)
+    gun = Gun(screen)
 
     while True:
         # Events proccessing.
@@ -15,8 +17,10 @@ def run() -> None:
                 sys.exit()
 
         screen.fill(bg_color)
+        gun.output()
         pygame.display.flip()
 
 
 if __name__ == '__main__':
     run()
+
